87487b7fe0f5f5fb27808a02f71d4e45f321939c
aa <xinyimin@126.com>
2022-07-08 11:30:47 +0800

    ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import './style.css'
 
 class App {
   constructor() {
-    let canvas = document.createElement("canvas")
+    let canvas:HTMLCanvasElement = document.createElement("canvas")
     canvas.style.width="100%"
     canvas.style.height="100%"
     canvas.id="gameCanvas"
     document.body.appendChild(canvas)
 
-    let engine = new BABYLON.Engine(canvas, true)
-    let scene = new BABYLON.Scene(engine)
+    let engine:BABYLON.Engine = new BABYLON.Engine(canvas, true)
+    let scene:BABYLON.Scene = new BABYLON.Scene(engine)
 
     let camera:BABYLON.ArcRotateCamera = new BABYLON.ArcRotateCamera("camera",Math.PI/2,Math.PI/2,2,BABYLON.Vector3.Zero(),scene)
     camera.attachControl(canvas, true)
